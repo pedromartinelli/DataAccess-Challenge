0ad6ae395a7e324855a229f794086b85daa9793d
Pedro Martinelli <p.martinellinunes@gmail.com>
2024-03-27 14:13:29 -0300

    feat(Model/Post): create post model

---
+++ Models/Post.cs
@@ -0,0 +1,28 @@
+﻿namespace Blog_Challenge.Models
+{
+    public class Post : Base
+    {
+        public Post(int categoryId, int authorId, string title, string summary, string body, string slug) : base(slug)
+        {
+            CategoryId = categoryId;
+            AuthorId = authorId;
+            Title = title;
+            Summary = summary;
+            Body = body;
+            Slug = slug;
+        }
+
+        public int CategoryId { get; set; }
+        public Category Category { get; set; } = null!;
+
+        public int AuthorId { get; set; }
+        public User Author { get; set; } = null!;
+
+        public string Title { get; set; }
+        public string Summary { get; set; }
+        public string Body { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime LastUpdateDate { get; set; }
+        public List<Tag> Tags { get; set; } = new List<Tag>();
+    }
+}
