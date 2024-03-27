be4b8066665b23cf648782facfee909fe45caceb
Pedro Martinelli <p.martinellinunes@gmail.com>
2024-03-27 14:53:37 -0300

    feat(Repositories/Repository): create generic crud repository

---
+++ Repositories/Repository.cs
@@ -0,0 +1,44 @@
+﻿using Blog_Challenge.Models;
+using Dapper.Contrib.Extensions;
+using Microsoft.Data.SqlClient;
+
+namespace Blog_Challenge.Repositories
+{
+    public class Repository<TModel> where TModel : Base
+    {
+        private readonly SqlConnection _connection;
+
+        public Repository(SqlConnection connection)
+        {
+            _connection = connection;
+        }
+
+        public void Create(TModel model)
+        {
+            model.Id = 0;
+            _connection.Insert(model);
+        }
+
+        public IEnumerable<TModel> GetAll()
+            => _connection.GetAll<TModel>();
+
+        public TModel Get(int id)
+            => _connection.Get<TModel>(id);
+
+        public void Update(TModel model)
+        {
+            if (model.Id == 0) return;
+
+            _connection.Update(model);
+        }
+
+        public void Delete(int id)
+        {
+            if (id == 0) return;
+
+            var user = _connection.Get<TModel>(id);
+            _connection.Delete<TModel>(user);
+        }
+
+    }
+}
