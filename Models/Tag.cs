3643afbe921ce2e87a589141fa54f26dc9c7d5fb
Pedro Martinelli <p.martinellinunes@gmail.com>
2024-03-27 14:13:46 -0300

    feat(Model/Tag): create tag model

---
+++ Models/Tag.cs
@@ -0,0 +1,13 @@
+﻿namespace Blog_Challenge.Models
+{
+    public class Tag : Base
+    {
+        public Tag(string name, string slug) : base(name, slug)
+        {
+            Name = name;
+            Slug = slug;
+        }
+
+        public List<Post> Posts { get; set; } = new List<Post>();
+    }
+}
