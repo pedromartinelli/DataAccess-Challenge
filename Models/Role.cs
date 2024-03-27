710f66f936c1f689eddfaeabd91ac1924ec6d8c2
Pedro Martinelli <p.martinellinunes@gmail.com>
2024-03-27 14:13:37 -0300

    feat(Model/Role): create role model

---
+++ Models/Role.cs
@@ -0,0 +1,11 @@
+﻿namespace Blog_Challenge.Models
+{
+    public class Role : Base
+    {
+        public Role(string name, string slug) : base(name, slug)
+        {
+            Name = name;
+            Slug = slug;
+        }
+    }
+}
