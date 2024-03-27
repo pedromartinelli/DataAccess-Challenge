420a3b0cec652007c9111b3067501c4fb7f52814
Pedro Martinelli <p.martinellinunes@gmail.com>
2024-03-27 14:12:54 -0300

    feat(Models/User): create user model

---
+++ Models/User.cs
@@ -0,0 +1,23 @@
+﻿namespace Blog_Challenge.Models
+{
+    public class User : Base
+    {
+        public User(string name, string email, string passwordHash, string bio, string image, string slug) : base(name, slug)
+        {
+            Name = name;
+            Email = email;
+            PasswordHash = passwordHash;
+            Bio = bio;
+            Image = image;
+            Slug = slug;
+        }
+
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public string Bio { get; set; }
+        public string Image { get; set; }
+
+        public List<Role> Roles { get; set; } = new List<Role>();
+        public List<Post> Posts { get; set; } = new List<Post>();
+    }
+}
