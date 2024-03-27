b6eb0e7594413780a81e57d2cb2d54f6d122fd24
Pedro Martinelli <p.martinellinunes@gmail.com>
2024-03-27 14:13:17 -0300

    feat(Model/Category): create category model

---
+++ Models/Category.cs
@@ -0,0 +1,13 @@
+﻿namespace Blog_Challenge.Models
+{
+    public class Category : Base
+    {
+        public Category(string name, string slug) : base(name, slug)
+        {
+            Name = name;
+            Slug = slug;
+        }
+
+        public List<Post> Posts { get; set; } = new List<Post>();
+    }
+}
